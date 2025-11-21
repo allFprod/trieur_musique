5d6dee7c2e377793d75f3fe37138d9f9f9937e1a
allFprod <antoine.gonnaud@neuf.fr>
2025-11-21 18:37:02 +0100

    Version commenté
    MAJ : script -> version commenté du scripte
    
    MAJ : README.md -> ajout de la mention "fonctionne sur windows"

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Voici un petit projet personnel, fait pour mon père ! 
 
+FONCTIONNE SUR WINDOWS (pas de version linux pour l'instant)
+
 ce programme python récupère tout les fichiers wav, mp3 et m4a (n'hésitez pas à en ajouter pour votre utilisation) et les tries par artiste dans un fichier du nom de celui-ci crée par python
 
 en cas d'erreur ou de non trie cela peut être dût aux charactère spéciaux ou extension
--- trimusique.py
+++ trimusique.py
@@ -1,36 +1,49 @@
 import subprocess
 
-subprocess.run(["pip", "install", "tinytag"])
+subprocess.run(["pip", "install", "tinytag"]) #install la librairie tinytag
 
 import shutil, os
 from tinytag import TinyTag
 from tqdm import tqdm
 
 def trie(repertoir) :
+    """
+    Entrée : répertoir d'action
+    récupère tout les fichiers wav, mp3 et
     
-    lst = os.listdir(repertoir)
+    et les tries par artiste dans un fichier du nom de celui-ci crée par python
+    """
     
-    if 'autre' not in lst :
+    lst = os.listdir(repertoir) #ajoute dans une liste (python) la liste des noms fichiers présent dans le répertoir
+    
+    if 'sans_nom' not in lst :
         print(lst)
         os.mkdir(repertoir + "sans_nom")
+        #crée un fichié sans_nom si il n'est pas présent
     
     
-    for f in tqdm(lst) :
+    for f in tqdm(lst) : # lis les noms des fichiers
+        #remplace les caractères gênants
         f = f.replace('é','e')
         f = f.replace('à','afe')
         f = f.replace('$','s')
         f = f.replace('@','a')
+        #mets le nom en minuscule
         f = f.lower()
+        
+        #récupère le nom de l'artiste
         tag: TinyTag = TinyTag.get(repertoir + f)
         artist = tag.artist
-        if '.WAV' in f or '.mp3' in f or 'm4a' in f:
-            if artist is None :
+        
+        if '.WAV' in f or '.mp3' in f or 'm4a' in f: #si le nom est un fichier musique alors
+            
+            if artist is None : #si il n'y a pas d'artiste : ranger dans le répertoir "sans_nom"
                 shutil.move(repertoir + "" + f, repertoir + "sans_nom")
-            elif artist not in os.listdir(repertoir) :
+            elif artist not in os.listdir(repertoir) : #sinon si il n'y a pas de répertoir au nom de l'artiste le crée puis déplacer
                 os.mkdir(repertoir + artist)
                 shutil.move(repertoir + f, repertoir + artist)
             else :
-                shutil.move(repertoir + f, repertoir + artist)
+                shutil.move(repertoir + f, repertoir + artist) #sinon déplacer
         
         
 
